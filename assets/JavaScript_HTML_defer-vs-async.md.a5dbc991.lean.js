607dab5fe3968e57f1af2ba929d53d4c8167cd89
wanzhihui <wanzhihui@yuanfudao.com>
2022-02-28 22:37:08 +0800

    deploy

---
+++ assets/JavaScript_HTML_defer-vs-async.md.a5dbc991.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as t,a as n}from"./app.572e84af.js";const g='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"JavaScript/HTML/defer-vs-async.md","lastUpdated":1645368770000}',o={},p=n("",2),e=[p];function r(c,l,i,_,k,d){return t(),a("div",null,e)}var f=s(o,[["render",r]]);export{g as __pageData,f as default};
